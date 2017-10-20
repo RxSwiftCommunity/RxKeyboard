c2a372aa46f7fe4bcd39b53a35ac6470f69180bc
Tomoya Hirano <noppelabs@gmail.com>
2017-10-20 12:23:11 +0900

    add shared scheme xcodeproj

---
+++ Carthage/RxKeyboard/RxKeyboard/RxKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  RxKeyboard.h
+//  RxKeyboard
+//
+//  Created by Tomoya Hirano on 2017/10/20.
+//  Copyright © 2017年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxKeyboard.
+FOUNDATION_EXPORT double RxKeyboardVersionNumber;
+
+//! Project version string for RxKeyboard.
+FOUNDATION_EXPORT const unsigned char RxKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxKeyboard/PublicHeader.h>
+
+
