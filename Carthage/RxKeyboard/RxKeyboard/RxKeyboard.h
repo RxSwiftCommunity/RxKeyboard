6f5406b67cfff03bcbb1ab3e3944f6949bb32c27
tomoya.hirano <tomoya.hirano@dena.com>
2018-09-07 20:10:37 +0900

    support no-use-binary

---
+++ Carthage/RxKeyboard/RxKeyboard/RxKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  RxKeyboard.h
+//  RxKeyboard
+//
+//  Created by Tomoya Hirano on 2018/09/07.
+//  Copyright © 2018年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxKeyboard.
+FOUNDATION_EXPORT double RxKeyboardVersionNumber;
+
+//! Project version string for RxKeyboard.
+FOUNDATION_EXPORT const unsigned char RxKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxKeyboard/PublicHeader.h>
+
+
