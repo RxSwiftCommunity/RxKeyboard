b5739549a88ba5786083ae877da8af4f505d7896
Vlad <Suhomlinov.dev@gmail.com>
2021-01-03 13:03:16 +0300

    Update Package.swift - add platforms

--- Package.swift
+++ Package.swift
@@ -3,14 +3,17 @@
 import PackageDescription
 
 let package = Package(
-  name: "RxKeyboard",
-  products: [
-    .library(name: "RxKeyboard", targets: ["RxKeyboard"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-  ],
-  targets: [
-    .target(name: "RxKeyboard", dependencies: ["RxSwift", "RxCocoa"]),
-  ]
+    name: "RxKeyboard",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "RxKeyboard", targets: ["RxKeyboard"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+    ],
+    targets: [
+        .target(name: "RxKeyboard", dependencies: ["RxSwift", "RxCocoa"]),
+    ]
 )
