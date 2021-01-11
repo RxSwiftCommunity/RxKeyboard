fd41857dcf8687d40a37a4c0d757a90bd3c4e8a5
Vlad <Suhomlinov.dev@gmail.com>
2021-01-11 12:24:15 +0300

    Code correction in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,17 +3,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "RxKeyboard",
-    platforms: [
-        .iOS(.v9)
-    ],
-    products: [
-        .library(name: "RxKeyboard", targets: ["RxKeyboard"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-    ],
-    targets: [
-        .target(name: "RxKeyboard", dependencies: ["RxSwift", "RxCocoa"]),
-    ]
+  name: "RxKeyboard",
+  products: [
+    .library(name: "RxKeyboard", targets: ["RxKeyboard"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+  ],
+  targets: [
+    .target(name: "RxKeyboard", dependencies: ["RxSwift", "RxCocoa"]),
+  ]
 )
